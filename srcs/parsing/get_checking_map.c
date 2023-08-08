cebcd9084f1c04cbfd2b29dc51ee6bb62618ae8d
Jules <julesthuys@gmail.com>
2023-08-09 00:30:02 +0200

    map filled w 'X'

--- srcs/parsing/get_checking_map.c
+++ srcs/parsing/get_checking_map.c
@@ -6,7 +6,7 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 22:42:46 by jthuysba          #+#    #+#             */
-/*   Updated: 2023/08/08 20:22:17 by jthuysba         ###   ########.fr       */
+/*   Updated: 2023/08/09 00:29:24 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,6 @@ int	fill_top_limit(t_parsing *data)
 {
 	int	i;
 
-	printf("%d\n", data->width);
 	data->map[0] = malloc(sizeof(char) * (data->width + 2));
 	if (!data->map[0])
 		return (1);
@@ -54,7 +53,7 @@ int	fill_top_limit(t_parsing *data)
 }
 
 //Copie une ligne du file dans la map en l'entourant de 'X'
-void	fill_line(char *map, char *line)
+void	fill_line(char *map, char *line, t_parsing *data)
 {
 	int	i;
 	int	j;
@@ -62,14 +61,24 @@ void	fill_line(char *map, char *line)
 	map[0] = 'X';
 	i = 1;
 	j = 0;
+	while (line[j] && line[j] == ' ')
+	{
+		map[i] = 'X';
+		i++;
+		j++;
+	}
 	while (line[j] && line[j] != '\n')
 	{
 		map[i] = line[j];
 		i++;
 		j++;
 	}
-	map[i] = 'X';
-	map[i + 1] = 0;
+	while (i < data->width + 1)
+	{
+		map[i] = 'X';
+		i++;
+	}
+	map[i] = 0;
 }
 
 //Rempli la derniere ligne de la map avec des 'X'
@@ -111,8 +120,8 @@ int get_checking_map(t_parsing *data)
 	i = 1;
 	while (line)
 	{
-		data->map[i] = malloc(sizeof(char) * (ft_strlen(line) + 2));
-		fill_line(data->map[i], line);
+		data->map[i] = malloc(sizeof(char) * (data->width + 2));
+		fill_line(data->map[i], line, data);
 		i++;
 		free(line);
 		line = get_next_line(fd);
